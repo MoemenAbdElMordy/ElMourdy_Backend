c4861474a8c51d1fabe56614f92fd7a8fe753380
MoemenAbdElMordy <abdelmordymoemen@gmail.com>
2026-07-28 16:13:28 +0300

    Scope English-only check to project files

--- test/policies/english_only_test.rb
+++ test/policies/english_only_test.rb
@@ -8,6 +8,9 @@ class EnglishOnlyTest < ActiveSupport::TestCase
   TEXT_FILENAMES = %w[
     .dockerignore .gitignore .ruby-version Dockerfile Gemfile Rakefile
   ].freeze
+  EXCLUDED_DIRECTORIES = %w[
+    .git log storage tmp vendor
+  ].freeze
 
   test "backend source and documentation use English only" do
     violations = project_text_files.filter_map do |path|
@@ -24,8 +27,10 @@ class EnglishOnlyTest < ActiveSupport::TestCase
 
   def project_text_files
     Rails.root.glob("**/*", File::FNM_DOTMATCH).select do |path|
+      relative_path = path.relative_path_from(Rails.root)
+
       path.file? &&
-        !path.to_s.include?("/.git/") &&
+        EXCLUDED_DIRECTORIES.exclude?(relative_path.each_filename.first) &&
         (TEXT_EXTENSIONS.include?(path.extname) || TEXT_FILENAMES.include?(path.basename.to_s))
     end
   end
